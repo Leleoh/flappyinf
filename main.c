184c9e08eab0e2b44b1f1c434c668ef04ec641f9
Leonel Ferraz <leonelfhernandez@outlook.com>
2024-01-19 21:21:41 -0300

    Animação do pássaro batendo asas

--- main.c
+++ main.c
@@ -13,10 +13,30 @@ int main()
     //Taxa de atualização do jogo
     SetTargetFPS(60);
 
+    //Carregando os frames do pássaro
+    Texture2D asa1 = LoadTexture("C:/Users/leone/Desktop/flappyinf/Recursos/asa1.png");
+    Texture2D asa2 = LoadTexture("C:/Users/leone/Desktop/flappyinf/Recursos/asa2.png");
+    Texture2D asa3 = LoadTexture("C:/Users/leone/Desktop/flappyinf/Recursos/asa3.png");
+
+    //Variáveis de controle para animação das asas
+    int frameAtual = 0;
+    int timer = 0;
+    int frameSpeed = 10;
+
     //Loop Principal
     while(!WindowShouldClose()) //Detecta o fechamento da janela
         {
         //Atualizar
+
+        //Asas batendo
+        timer ++;
+        if (timer >= frameSpeed){
+            frameAtual++;
+            if (frameAtual > 2){
+                frameAtual = 0;
+            }
+            timer = 0;
+        }
 
         //Desenhar
         BeginDrawing();
@@ -25,11 +45,24 @@ int main()
         //Desenhar o plano de fundo
         DrawTexture(background, 0, 0, WHITE);
 
+        //Asas batendo
+        switch(frameAtual){
+        case 0:
+            DrawTexture(asa1, 0, 0, WHITE);
+            break;
+        case 1:
+            DrawTexture(asa2, 0, 0, WHITE);
+            break;
+        case 2:
+            DrawTexture(asa3, 0, 0, WHITE);
+            break;
+        }
+
+
         //Futuros elementos
 
 
         EndDrawing();
-
 
 
     }
